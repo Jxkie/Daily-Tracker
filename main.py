2dc2b7be716b01749ef77b3f96e6483a1ad24521
Antarctica ‎ <jerz.rucki@gmail.com>
2026-04-06 14:31:10 +0200

    Trying json and simple func

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-arr = [1,2,3]
-print(arr)
+import json
+
+taskOfDay = []
+
+try:
+    with open("tasks.json", "r") as f:
+        taskOfDay = json.load(f)
+except:
+    with open("tasks.json", "x") as f:
+        pass
+
+def addTask(title, time):
+    task = {
+        "taskName": title,
+        "taskTime": time,
+        "taskStatus": False
+    }
+    taskOfDay.append(task)
+    return taskOfDay
+
+title = input("Название задачи:")
+time = input("Время задачи:")
+
+addTask(title, time)
+
+with open("tasks.json", "w") as f:
+    json.dump(taskOfDay, f)
+
+print(taskOfDay)
